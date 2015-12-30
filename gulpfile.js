082fd9af15f3d1d616f23525abcf5942342489aa
Andrew Jacobson <andyjacuw@gmail.com>
2015-12-29 16:53:33 -0800

    Add build process for client code

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var plumber = require('gulp-plumber');
 var nodemon = require('gulp-nodemon');
+var merge = require('merge-stream');
+var webpack = require('gulp-webpack');
 
 gulp.task('lint:server', function() {
   return gulp.src(['./app.js', './gulpfile.js'])
@@ -13,14 +15,43 @@ gulp.task('lint:server', function() {
 
 gulp.task('nodemon', ['lint:server'], function() {
   return nodemon({
-    script: './app.js',
-    ignore: ['./public/**/']
+    script: 'app.js',
+    ext: 'js'
   })
     .on('restart', function() {
       console.log('server restarting...');
     });
 });
 
+gulp.task('client', function() {
+  var html = gulp.src('./public/**/*.html')
+      .pipe(gulp.dest('./dist'));
+
+  var build = gulp.src('./public/js/index.js')
+      .pipe(webpack({
+        module: {
+          loaders: [{
+            test: /\.js$/,
+            loader: 'babel',
+            query: {
+              cacheDirectory: true,
+              presets: ['es2015', 'react']
+            }
+          }]
+        },
+        output: {
+          filename: 'bundle.js'
+        }
+      }))
+      .pipe(gulp.dest('./dist'));
+
+
+  merge(html, build);
+});
+
 gulp.task('watch', function() {
   gulp.watch('./app.js', ['lint:server']);
+  gulp.watch('./public/**/*', ['client']);
 });
+
+gulp.task('default', ['client', 'watch']);
